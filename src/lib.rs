6148d9883d82c698a2946066782b3a98cc66e1be
un3481 <78743473+un3481@users.noreply.github.com>
2024-05-25 21:16:26 -0300

    Make lib a single file

--- src/lib.rs
+++ src/lib.rs
@@ -25,12 +25,10 @@
 //! }
 //! ```
 
-use connection::Connection;
+use std::io::Read;
+use spidev::{ Spidev, SpidevOptions, SpiModeFlags };
+use simmer::Temperature;
 use thiserror::Error;
-
-pub mod connection;
-
-pub use simmer::Temperature;
 
 /// An error emitted due to problems with the MAX6675.
 #[derive(Debug, Error)]
@@ -42,12 +40,14 @@ pub enum Max6675Error {
     },
     #[error("The MAX6675 detected an open circuit (bit D2 was high). Please check the thermocouple connection and try again.")]
     OpenCircuitError,
+    #[error("The SPI connection to the MAX6675 has not been completed, plese run `connect()` and try again.")] 
+    SpiUninitialized,
 }
 
 /// A representation of the MAX6675 thermocouple digitizer.
 #[derive(Debug)]
 pub struct Max6675 {
-    connection: Connection,
+    spi: Option<Spidev>
 }
 
 impl Max6675 {
@@ -68,58 +68,23 @@ impl Max6675 {
     ///     }
     /// }
     /// ````
-    pub fn new(spi_connection_path: impl AsRef<str>) -> Result<Self, Max6675Error> {
-        Ok(Self {
-            connection: Connection::new(spi_connection_path)?,
-        })
+    pub const fn new() -> Self {
+        Self { spi: None }
     }
 
-    /// Tries to read the thermocouple's temperature in Celsius.
-    ///
-    /// ## Example
-    ///
-    /// ```no_run
-    /// # use linux_max6675::Max6675;
-    /// # fn main() -> anyhow::Result<()> {
-    /// let mut thermo = Max6675::new("/dev/spidev0.0")?;
-    /// println!("it's {}° celsius in here!", thermo.read_celsius()?);
-    /// # Ok(())
-    /// # }
-    /// ```
-    pub fn read_celsius(&mut self) -> Result<Temperature, Max6675Error> {
-        Ok(Temperature::Celsius(self.connection.read_as_celsius()?))
-    }
-
-    /// Tries to read the thermocouple's temperature in Fahrenheit.
-    ///
-    /// ## Example
-    ///
-    /// ```no_run
-    /// # use linux_max6675::Max6675;
-    /// # fn main() -> anyhow::Result<()> {
-    /// let mut thermo = Max6675::new("/dev/spidev0.0")?;
-    /// println!("oh man! it's {}° fahrenheit in here!", thermo.read_fahrenheit()?);
-    /// # Ok(())
-    /// # }
-    /// ```
-    pub fn read_fahrenheit(&mut self) -> Result<Temperature, Max6675Error> {
-        Ok(self.read_celsius()?.to_fahrenheit())
-    }
-
-    /// Tries to read the thermocouple's temperature in Kelvin.
-    ///
-    /// ## Example
-    ///
-    /// ```no_run
-    /// # use linux_max6675::Max6675;
-    /// # fn main() -> anyhow::Result<()> {
-    /// let mut thermo = Max6675::new("/dev/spidev0.0")?;
-    /// println!("i'm a nerd and i use kelvin! and it's {}° kelvin!", thermo.read_kelvin()?);
-    /// # Ok(())
-    /// # }
-    /// ```
-    pub fn read_kelvin(&mut self) -> Result<Temperature, Max6675Error> {
-        Ok(self.read_celsius()?.to_kelvin())
+    /// Tries to create a new `Spidev` connection.
+    /// Only fails if there's something wrong with the SPI connection.
+    pub fn connect(&mut self, spi_path: impl AsRef<str>) -> Result<&mut Self, Max6675Error> {
+        let mut spi = Spidev::open(spi_path.as_ref())?;
+        spi.configure(
+            &SpidevOptions::new()
+                .bits_per_word(8)
+                .max_speed_hz(1_000_000)
+                .mode(SpiModeFlags::SPI_MODE_1)
+                .build(),
+        )?;
+        self.spi = Some(spi);
+        Ok(self)
     }
 
     /// Tries to return the thermocouple's raw data for data science. (and other fun little things)
@@ -138,7 +103,37 @@ impl Max6675 {
     /// # Ok(())
     /// # }
     /// ````
-    pub fn read_raw(&mut self) -> Result<[u8; 2], Max6675Error> {
-        self.connection.read_raw()
+    pub fn read_bytes(&mut self) -> Result<u16, Max6675Error> {
+        let mut buf = [0_u8; 2];
+        self.spi.ok_or(Err(Max6675Error::SpiUninitialized))?
+            .read_exact(&mut buf)?;
+        Ok(u16::from_be_bytes(buf))
+    }
+
+    /// Tries to grab temperature data from MAX6675 and convert the results
+    /// to an `f64` in degrees Celsius.
+    pub fn read_float(&mut self) -> Result<f64, Max6675Error> {
+        let bytes = self.read_bytes()?;
+        // check for Bit D2 being high, indicating that the thermocouple input is open
+        // (see MAX6675 datasheet, p. 5)
+        if bytes & 0x04 { Err(Max6675Error::OpenCircuitError)? };
+        // ripped from the Arduino library (see: https://github.com/RobTillaart/MAX6675)
+        Ok((0x1FFF & (bytes >> 3)) as f64) * 0.25)
+    }
+
+    /// Tries to read the thermocouple's temperature in Celsius.
+    ///
+    /// ## Example
+    ///
+    /// ```no_run
+    /// # use linux_max6675::Max6675;
+    /// # fn main() -> anyhow::Result<()> {
+    /// let mut thermo = Max6675::new("/dev/spidev0.0")?;
+    /// println!("it's {}° celsius in here!", thermo.read_celsius()?);
+    /// # Ok(())
+    /// # }
+    /// ```
+    pub fn read_celsius(&mut self) -> Result<Temperature, Max6675Error> {
+        Ok(Temperature::Celsius(self.read_float()?))
     }
 }
