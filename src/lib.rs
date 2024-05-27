d99d4031da865179ff6a157a6dbd624a12ffa392
un3481 <78743473+un3481@users.noreply.github.com>
2024-05-27 15:40:01 -0300

    fix move error

--- src/lib.rs
+++ src/lib.rs
@@ -125,6 +125,7 @@ impl Max6675 {
         // Read MISO bytes into buffer
         let mut buf = [0_u8; 2];
         self.spi
+            .map(|spi| spi.as_ref())
             .ok_or(Max6675Error::SpiUninitialized)?
             .read_exact(&mut buf)?;
         // Convert buffer into word
